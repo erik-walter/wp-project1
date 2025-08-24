390a77e174a0fffaf866dc9008e96a68b68b7a15
Crudelita <Katha.Walter97@web.de>
2025-08-24 18:27:49 +0200

    portfolio

--- wp-content/themes/custom-theme/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function my_custom_theme_setup() {
-    // Theme-Support hinzufügen
-    add_theme_support('title-tag'); // dynamischer Titel
-    add_theme_support('post-thumbnails'); // Beitragsbilder
-}
-add_action('after_setup_theme', 'my_custom_theme_setup');
